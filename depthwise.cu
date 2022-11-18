f045f9a246d89551363cf2215c4bf653356fed84
Chong-Chen <chong.chen@unlv.edu>
2022-11-17 17:29:18 -0800

    some updates

--- depthwise.cu
+++ depthwise.cu
@@ -2,14 +2,64 @@
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
 
-__global__ void depthwise_kernel(float* inputTensor, 
+__global__ void depthwise_kernel_foward(float* inputTensor, 
 		float* weightTensor,
 		float* outputTensor,
-	   	const uint8_t width)
+	   	const int8_t width)
 {
 	extern __shared__ volatile float weightS[];
-	__shared__ volatile float inputCache[warpPerBlock*warpSize];
+	__shared__ volatile float inputCache[threadSize];
+	float outputCache[4];//8X16 or 4X32 output cache, with 32 warp per block.
+
+	while(){
+		inputCache[tid] = ;
+
+		while(tid + threadSize < weightSize){	
+			weightS[tid + threadSize] = weightG[tid + threadSize];	
+		}
+
+		while(){
+			inputCache[tid] = ;
+		}	
+
+		for(int8_t i = 0; i < width; ++i){
+
+		}
+	}
 	
+
+}
+
+__global__ void depthwise_kernel_backward_all(float* outputDelta,
+		float* weightTensor,
+		float* weightDeltaCache,
+		float* inputTensor,
+		float* inputDelta,
+		const int8_t width)
+{
 	
+	while(){
+
+	}	
+
+}
+//backward used sync frequently, may cause performance loss
+__global__ void depthwise_kernel_backward_weight(float* outputDelta,
+		float* weightDeltaCache,
+		float* inputTensor,
+		const int8_t width)
+{
+	float inputCache[4];	
+	extern __shared__ volatile float weightS[];
+	__shared__ volatile float outputS[];
+	while(){
+		outputS[] = outputDelta[];
+		while(){	
+			weightS[] += outputS[]*inputCache[] ;				
+			__
+		}
+		
+	}	
+
 
 }
